0a7202830e379a8e1773a085c875eb563aad7a65
Rian-Ismael <106334437+Rian-Ismael@users.noreply.github.com>
2023-04-13 21:17:42 -0300

    Update CountingSort.java

--- RoteiroEda1/src/Roteiro/CountingSort.java
+++ RoteiroEda1/src/Roteiro/CountingSort.java
@@ -6,43 +6,42 @@ public class CountingSort {
 		
 	}
 	
-	 public static void sort(int[] arr) {
-	        if (arr == null || arr.length == 0) {
-	            return;
+	 	public static void sort(int[] arr) {
+	        	if (arr == null || arr.length == 0) {
+	            	return;
 	        }
+		int max = arr[0];
+		int min = arr[0];
 
-	        int max = arr[0];
-	        int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+		    if (arr[i] > max) {
+			max = arr[i];
+		    }
+		    if (arr[i] < min) {
+			min = arr[i];
+		    }
+		}
 
-	        for (int i = 1; i < arr.length; i++) {
-	            if (arr[i] > max) {
-	                max = arr[i];
-	            }
-	            if (arr[i] < min) {
-	                min = arr[i];
-	            }
-	        }
+		int range = max - min + 1;
+		int[] count = new int[range];
 
-	        int range = max - min + 1;
-	        int[] count = new int[range];
+		for (int i = 0; i < arr.length; i++) {
+		    count[arr[i] - min] += 1;
+		}
 
-	        for (int i = 0; i < arr.length; i++) {
-	            count[arr[i] - min]++;
-	        }
+		for (int i = 1; i < range; i++) {
+		    count[i] += count[i - 1];
+		}
 
-	        for (int i = 1; i < range; i++) {
-	            count[i] += count[i - 1];
-	        }
+		int[] output = new int[arr.length];
 
-	        int[] output = new int[arr.length];
+		for (int i = arr.length - 1; i >= 0; i--) {
+		    output[count[arr[i] - min] - 1] = arr[i];
+		    count[arr[i] - min]--;
+		}
 
-	        for (int i = arr.length - 1; i >= 0; i--) {
-	            output[count[arr[i] - min] - 1] = arr[i];
-	            count[arr[i] - min]--;
-	        }
 
-	        for (int i = 0; i < arr.length; i++) {
-	            arr[i] = output[i];
-	        }
-	    }
+		System.out.println(Arrays.toString(output));
+		
+	}
 }
