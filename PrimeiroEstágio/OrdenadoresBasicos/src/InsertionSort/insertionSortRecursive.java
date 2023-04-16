c2ea4fe7178dc4525c71c8f363e0d1171e926937
Rian-Ismael <106334437+Rian-Ismael@users.noreply.github.com>
2023-04-15 23:35:27 -0300

    Update insertionSortRecursive.java

--- PrimeiroEstágio/OrdenadoresBasicos/src/InsertionSort/insertionSortRecursive.java
+++ PrimeiroEstágio/OrdenadoresBasicos/src/InsertionSort/insertionSortRecursive.java
@@ -19,20 +19,4 @@ public class insertionSortRecursive {
 	        } 
 	    }
 	}
-	
-	public static void insertionRecursivo(int[] array, int leftIndex, int rightIndex) {
-		if(leftIndex < rightIndex) {
-			insertionRecursivo(array, leftIndex, rightIndex-1);
-			
-			int key = array[rightIndex];
-			int j = rightIndex-1;
-			
-			while(j >= leftIndex && array[j] > key) {
-				int aux = array[j];
-				array[j] = array[j+1];
-				array[j+1] = aux;
-				j--;
-			}
-		}
-	}
 }
